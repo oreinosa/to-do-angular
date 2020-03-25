81b115bd82c72de3f2282dd7161b434545c4d9d3
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-25 10:34:54 -0600

    -run getAll query for notes and lists in app.component once user is loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { Observable } from 'rxjs';
 import { Link } from './shared/models/link';
 import { User } from './shared/models/user';
+import { NotesService } from './notes/notes.service';
+import { ListsService } from './lists/lists.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'to-do-angular';
   links$: Observable<Link[]>;
   user$: Observable<User>;
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private notes: NotesService,
+    private lists: ListsService
   ) {
+
+  }
+
+  ngOnInit() {
     this.links$ = this.auth.getLinks();
-    this.user$ = this.auth.getUser();
+    this.user$ = this.auth.getUser()
+      .pipe(
+        tap(async user => {
+          if (user) {
+            const promises = [this.notes.getAll(), this.lists.getAll()];
+            await Promise.all(promises);
+          }
+        }));
   }
 
   onLogout() {
